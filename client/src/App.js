8ae5a2bfe784397ac12ee0ffbdbbb3cd46adfb00
Toegram <TTuretsky@gmail.com>
2021-08-04 09:28:44 -0400

    fixed git issue

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PostCreate from "./Components/PostCreate.js";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <h3> This is a test for committing changes</h3>
+      <PostCreate />
+    </div>
+  );
+};
+
+export default App;
